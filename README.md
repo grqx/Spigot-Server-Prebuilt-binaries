b4d0c6d7be2545a6a6dbb63ccd6bcf6e17644411
Zenith Wang_VSCode <173253225+grqx@users.noreply.github.com>
2024-11-23 14:48:28 +1300

    build

---
+++ README.md
@@ -0,0 +1,24 @@
+# Spigot-Server-Prebuilt-binaries 
+
+> [!NOTE]  
+> This repo has been migrated and the commit history/releases has been lost.
+
+This repo is not often updated, some binaries can be downloaded from [the official website](https://getbukkit.org/download/spigot) instead.
+
+This is the spigot server builder, only for versions newer than 1.8. For older versions, please check [Jenkins CI](https://ci.md-5.net/job/Spigot/).  
+
+**Caution**: Due to the log4j2 security issue, some of the prebuilt versions **ARE NOT** safe because they are not patched although they are built recently, see [the official thread](https://www.spigotmc.org/threads/spigot-security-releases-%E2%80%94-1-8-8%E2%80%931-18.537204/).  
+The following versions have been patched:  
+
+    1.8.8 (BuildTools rev 582-a)
+    1.9.4 (BuildTools rev 849-a)
+    1.10.2 (BuildTools rev 986-a)
+    1.11.2 (BuildTools rev 1251-a)
+    1.12.2 (BuildTools rev 1573-k)
+    1.13.2 (BuildTools rev 2148-d)
+    1.14.4 (BuildTools rev 2502-c)
+    1.15.2 (BuildTools rev 2703-a)
+    1.16.5 (BuildTools rev 3096-a)
+    1.17.1 (BuildTools rev 3284-a)
+    1.18 (all versions after 10 December)
+    1.18.1 (all versions)
---
+++ build.sh
@@ -0,0 +1,36 @@
+echo '[build] Downloading openjdk-21.0.2 archive ...'
+curl -L https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz --output openjdk-21.0.2.tar.gz
+if ! [ -e openjdk-21.0.2.tar.gz ] ;then
+    >&2 echo '[build] Failed to download openjdk-21.0.2.tar.gz! The script is going to exit...'
+    exit
+fi
+tar -zxf openjdk-21.0.2.tar.gz
+
+
+echo '[build] Downloading BuildTools.jar ...'
+curl -L https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar --output BuildTools.jar
+if ! [ -e BuildTools.jar ] ;then
+    >&2 echo '[build] Failed to download BuildTools.jar! The script is going to exit...'
+    exit
+fi
+
+echo '[build] Creating workspace for Spigot Builder ...'
+2>/dev/null mkdir SPBuild
+mv -f BuildTools.jar SPBuild >/dev/null
+cd SPBuild
+
+echo '[build] Building Spigot...'
+../jdk-21.0.2/bin/java -jar BuildTools.jar --rev ${BUILD_VERSION:-1.21}
+if ! [ -e spigot-*.jar ] ;then
+    >&2 echo '[build] Error Building Spigot! The script is going to exit...'
+    exit
+fi
+echo '[build] Build success!'
+cd ..
+
+echo '[build] Exporting built artifacts ...'
+mv -f SPBuild/spigot-*.jar . >/dev/null
+echo '[build] Removing src ...'
+rm -rf SPBuild
+echo '[build] Renaming artifact ...'
+mv -f spigot-*.jar spigot.jar
